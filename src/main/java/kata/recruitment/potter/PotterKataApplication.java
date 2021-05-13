3c97275e339f8040806f51a2926fd32597b122cf
Shoun Ichida <shoun@ichida.fr>
2021-05-13 14:54:44 +0200

    Initial commit with spring boot starter

---
+++ src/main/java/kata/recruitment/potter/PotterKataApplication.java
@@ -0,0 +1,13 @@
+package kata.recruitment.potter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotterKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotterKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kata/recruitment/potter/PotterKataApplicationTests.java
@@ -0,0 +1,13 @@
+package kata.recruitment.potter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotterKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
